32d3509e775f4ae9b08a73d7d70e15c5dd23f93b
Леха <aleksejgris8@gmail.com>
2025-04-21 19:40:59 +0200

    диплом

--- Final_test/test/test_api.py
+++ Final_test/test/test_api.py
@@ -1,32 +1,78 @@
-import requests
+import allure
 
 
-def test_request_by_id():
-   url = 'https://api.kinopoisk.dev/v1.4/movie/1000'
-   params = {"id":"1000"
-   }
-   response = requests.get( url, headers= headers , params= params)
-   assert response.status_code == 200
+@allure.feature("Поиск фильма/сериала")
+@allure.title("Тест получения информации о фильме/сериале по названию")
+@allure.description("Получение информации о\
+                      фильме/сериале согласно введенным данным")
+@allure.id(1)
+@allure.severity("Blocker")
+def test_get_film_tv_series_by_name(api):
+   film_tv_series = "Сонная Лощина"
 
-def test_films_random():
-   url = 'https://api.kinopoisk.dev/v1.4/movie/random'
-   params = {
-   "year":"2021",
-   "month":"january",
-   "id":"666"
-   }
-   response = requests.get( url, headers=headers , params= params)
-   assert response.status_code == 200
+   result_search_by_name, status_code = api.search_film_tv_series_by_name(
+      film_tv_series
+   )
+   assert status_code == 200
+   assert result_search_by_name["docs"][1]["name"] == "Сонная Лощина"
 
 
+@allure.feature("Поиск фильма/сериала")
+@allure.title("Тест получения информации о фильме/сериале по id")
+@allure.description("Получение информации\
+                      о фильме/сериале согласно введенным данным")
+@allure.id(2)
+@allure.severity("Blocker")
+def test_get_film_tv_series_by_id(film_api):
+   film_id = "4958668"
 
-def test_search():
-   url = 'https://api.kinopoisk.dev/v1.4/movie/search'
-   params = {
-      "query":"Шрек",
-      "limit":"5",
-      "page":"1"
-   }
-   response = requests.get( url, headers=headers, params=params)
-   assert response.status_code == 200
-   assert "Шрек" in response.tex
+   result_search_by_id, status_code = film_api.search_film_tv_series_by_id(
+      film_id
+   )
+   assert status_code == 200
+   assert result_search_by_id["id"] == int(film_id)
+   assert result_search_by_id["name"] == "Триггер"
+
+
+@allure.feature("Поиск персоны.")
+@allure.title("Тест получение информации о персоне по имени и фамилии")
+@allure.description("Получение информации согласно введенным данным")
+@allure.id(4)
+@allure.severity("Blocker")
+def test_search_person_by_name(person_api):
+   person_name = "Джонни Депп"
+   result_serch_person_name, status_code = person_api.search_person_by_name(
+      person_name
+   )
+   assert status_code == 200
+   assert result_serch_person_name["docs"][0]["name"] == person_name
+   assert result_serch_person_name["docs"][0]["id"] == 6245
+
+
+@allure.feature("Поиск персоны.")
+@allure.title("Тест получения информации о персоне по id")
+@allure.description("Получение информации согласно введенным данным")
+@allure.id(5)
+@allure.severity("Blocker")
+def test_search_person_by_id(person_api):
+   person_id = "6245"
+   result_search_person_by_id, status_code = person_api.search_person_by_id(
+      person_id
+   )
+   assert status_code == 200
+   assert result_search_person_by_id["id"] == int(person_id)
+   assert result_search_person_by_id["name"] == "Джонни Депп"
+
+
+@allure.feature("Поиск фильма/сериала")
+@allure.title("Тест на получение информации о фильме/сериале по полям")
+@allure.description("Получение информации согласно введенным данным")
+@allure.id(3)
+@allure.severity("Critical")
+def test_get_film_tv_series_by_field(api):
+   film_tv_series_field = "genres.name"
+   result_search_by_field, status_code = api.search_by_fields(
+      film_tv_series_field
+   )
+   assert status_code == 200
+   assert "ужасы" in [genre["name"] for genre in result_search_by_field]
